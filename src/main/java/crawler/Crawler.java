f9ea38a1b31b4ad238094d062fcdbd7fd783e97e
201250040 <201250040@smail.nju.edu.cn>
2021-11-30 22:38:21 +0800

    finish Crawler but need to polish up

--- src/main/java/crawler/Crawler.java
+++ src/main/java/crawler/Crawler.java
@@ -3,7 +3,9 @@ package crawler;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v85.io.IO;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.BufferedWriter;
@@ -20,7 +22,7 @@ accountD = {13961496689, xjX2MS!WWPZdcT}
 
 public class Crawler {
     WebDriver driver;
-    int WAIT_SECONDS = 3;
+    int WAIT_SECONDS = 5;
 
     public Crawler() {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
@@ -40,28 +42,25 @@ public class Crawler {
 
         //login
         WebElement login = driver.findElement(By.id("loginLi"));
-        if (login.getText().contains("登录")) {
-            login(login, "15371939295", "Zjh320682@nju");
-            // 确保返回了首页
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            assert (!driver.getWindowHandle().equals(index));
-        }
-        // 登录成功
-        WebElement search = driver.findElement(By.cssSelector(".search-rightBtn.search-click"));
-        easyCrawl(search, "周岩松");
+        login(login, "15371939295", "Zjh320682@nju", index);
 
+        refresh();
+
+        easyCrawl("周岩松");
         //WebElement adSearch = driver.findElement(By.cssSelector(".advenced-search"));
         //crawlByDate(adSearch, new Date(110, 1, 1), new Date());
         download();
+
         driver.quit();
     }
 
-    private void login(WebElement loginButton, String account, String pswd) {
+    private void back2HomePage() {
+        driver.findElement(By.cssSelector(".case.souye")).click();
+    }
+
+    private void login(WebElement loginButton, String account, String pswd, String preIndex) {
         if (!loginButton.getText().contains("登录")) return;
+
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
@@ -87,6 +86,25 @@ public class Crawler {
         driver.findElement(By.cssSelector(".password")).sendKeys(pswd);
         // 登陆后跳转回首页
         driver.findElement(By.cssSelector("span.button-primary")).click();
+        refresh();
+
+        assert (!driver.getWindowHandle().equals(preIndex));
+        // 确保返回了首页
+        // 登录成功
+    }
+
+    private void refresh() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.navigate().refresh();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     private void crawlByDate(WebElement adSearch, Date start, Date end) {
@@ -100,7 +118,6 @@ public class Crawler {
         driver.findElement(By.cssSelector("#cprqEnd")).sendKeys(endDate);
         try {
             Thread.sleep(1000);
-            driver.navigate().refresh();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -108,12 +125,11 @@ public class Crawler {
         // finish search
     }
 
-    private void easyCrawl(WebElement search, String name) {
-
+    private void easyCrawl(String name) {
+        WebElement search = driver.findElement(By.cssSelector(".search-rightBtn.search-click"));
         driver.findElement(By.cssSelector(".searchKey.search-inp")).sendKeys(name);
         try {
             Thread.sleep(1000);
-            driver.navigate().refresh();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -126,26 +142,49 @@ public class Crawler {
 
     private void download(int num) {
         //检索之后，一个页面有5个记录
-        List<WebElement> pages = new WebDriverWait(driver, Duration.ofSeconds(WAIT_SECONDS))
+        refresh();
+        List<WebElement> pages = new WebDriverWait(driver, Duration.ofSeconds(WAIT_SECONDS*3))
                 .until((driver -> driver.findElements(By.cssSelector(".caseName"))));
-        driver.getWindowHandle();
-        for (WebElement page : pages) {
-            pageDownload(page, driver.getWindowHandle());
-        }
-        num -= pages.size();
-        if (num > 0) {
+        String handle = driver.getWindowHandle();
+
+        if (pages.size() < 5) {
+            for (WebElement page : pages) pageDownload(page, handle);
+            return;
+        } //如果单页的记录少于5个，说明就这么多
+        if (num > 5) {
+            for (WebElement page : pages) {
+                pageDownload(page, handle);
+            }
             WebElement nextPage = driver.findElement(By.linkText("下一页"));
             nextPage.click();
-            download(num);
+            download(num - pages.size());
+        } else {
+            for (int i = 0; i < num; i++) {
+                pageDownload(pages.get(i), handle);
+            }
         }
     }
 
     private void pageDownload(WebElement tab, String previosHandle) {
         tab.click();
-        WebElement pdfPox = new WebDriverWait(driver, Duration.ofSeconds(WAIT_SECONDS))
-                .until((driver -> driver.findElement(By.cssSelector(".PDF_pox")))); //not Box, pox is content
-        String pdfTitle = driver.findElement(By.cssSelector(".PDF_title")).getText();
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("./src/main/resources" + pdfTitle))) {
+        for (String windowHandle : driver.getWindowHandles()) {
+            if(!previosHandle.contentEquals(windowHandle)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+        refresh();
+        String pdfTitle = new WebDriverWait(driver, Duration.ofSeconds(WAIT_SECONDS))
+                .until(driver ->driver.findElement(By.cssSelector(".PDF_title")).getText());
+        //String pdfTitle = driver.findElement(By.cssSelector(".PDF_title")).getText();
+        WebElement pdfPox = driver.findElement(By.cssSelector(".PDF_pox"));
+        StringBuilder reason = new StringBuilder();
+        driver.findElements(By.cssSelector("#iframedf .text-ellipsis"))
+                .forEach(WebElement -> reason.append(WebElement.getText()));
+        reason.append(System.lineSeparator());
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("./src/main/resources/" + pdfTitle))) {
+            writer.write(reason.toString());
             writer.write(pdfPox.getAttribute("innerHTML"));
         } catch (IOException e) {
             e.printStackTrace();
